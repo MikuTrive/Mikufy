a5cb15c02bad0cdf3155701817197d08569ec057
miku <18223088921@163.com>
2026-02-05 05:32:48 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,5 @@
 #
-<<<<<<< Updated upstream
-# Mikufy v2.4(stable) - 一键清理脚本
-=======
 # Mikufy v2.5(stable) - 一键清理脚本
->>>>>>> Stashed changes
 # 清理编译构建产生的产物
 #
 
@@ -16,11 +12,7 @@ NC='\033[0m' # No Color
 
 # 项目信息
 PROJECT_NAME="Mikufy"
-<<<<<<< Updated upstream
-VERSION="2.4(stable)"
-=======
 VERSION="2.5(stable)"
->>>>>>> Stashed changes
 OUTPUT_FILE="mikufy"
 
 # 打印函数
